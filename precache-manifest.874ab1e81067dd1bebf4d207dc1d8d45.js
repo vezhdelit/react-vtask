0414548fb5fb88e1213dfaf2a619afc5e51b4714
vezhdelit <57722783+vezhdelit@users.noreply.github.com>
2022-05-22 23:25:46 +0300

    Updates

---
+++ precache-manifest.874ab1e81067dd1bebf4d207dc1d8d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf668b533e0b80892100",
+    "url": "/static/css/main.02db8c51.chunk.css"
+  },
+  {
+    "revision": "bf668b533e0b80892100",
+    "url": "/static/js/main.bf668b53.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ec442889baae5ba23725",
+    "url": "/static/js/2.ec442889.chunk.js"
+  },
+  {
+    "revision": "014939545987731d5730ec0ab1049e6f",
+    "url": "/index.html"
+  }
+];
